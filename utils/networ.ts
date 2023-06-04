740c8d640f077d8e0f6981756c49840b4830fb35
kapil.guleria074 <kapil.guleria074@gmail.com>
2023-06-04 09:55:10 +0530

    fix: change API Base_url

--- utils/networ.ts
+++ utils/networ.ts
@@ -1,4 +1,6 @@
-export const BASE_URL = `http://localhost:8000/api`
+// export const BASE_URL = `http://localhost:8000/api`
+export const BASE_URL = `https://project-be.onrender.com/api`
+
 
 // auth
 export const register = `/auth/register`
